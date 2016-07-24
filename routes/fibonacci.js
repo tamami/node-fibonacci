b0d1d722183fe793b4f6d8e0f1d0e18cc06b0225
tamami <tamami.oka@gmail.com>
2016-07-24 22:44:27 +0700

    init commit, sukses

---
+++ routes/fibonacci.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var math = require("../math");
+
+router.get('/', function(req, res, next) {
+  if(req.query.fibonum) {
+    res.render('fibonacci', {
+      title: "Calculate Fibonacci numbers",
+      fibonum: req.query.fibonum,
+      fiboval: math.fibonacci(req.query.fibonum)
+    });
+  } else {
+    res.render('fibonacci', {
+      title: "Calculate Fibonacci numbers",
+      fiboval: undefined
+    });
+  }
+
+});
+
+module.exports = router;
